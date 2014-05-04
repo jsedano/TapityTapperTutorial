b6b6532a58ba3b7400c335f841bf520a216fbdfd
Juan Carlos Sedano Salas <jc.sedano.isc@gmail.com>
2014-05-04 03:23:23 -0500

    http://roadtonerdvana.com/2014/05/04/tapity-tapper-my-ios-flappy-bird-clone-using-sprite-kit-game-over/

--- Tapity Tapper/Tapity Tapper/JCMyScene.m
+++ Tapity Tapper/Tapity Tapper/JCMyScene.m
@@ -11,6 +11,11 @@
 @interface JCMyScene() <SKPhysicsContactDelegate>{}
 
 @property SKSpriteNode *rectangle;
+@property SKAction *obstacleLoop;
+@property BOOL gameOver;
+@property UIAlertView *gameOverAlert;
+@property SKLabelNode *startGameLabel;
+
 typedef enum : uint8_t {
     JCColliderTypeRectangle = 1,
     JCColliderTypeObstacle  = 2
@@ -25,43 +30,60 @@ typedef enum : uint8_t {
 -(id)initWithSize:(CGSize)size {    
     if (self = [super initWithSize:size]) {
         /* Setup your scene here */
-        
         self.backgroundColor = [SKColor colorWithRed:0.15 green:0.15 blue:0.3 alpha:1.0];
         
         self.physicsBody = [SKPhysicsBody bodyWithEdgeLoopFromRect:CGRectMake(0, 0, self.size.width, self.size.height)];
     
         self.physicsWorld.contactDelegate = self;
+        
+        SKAction *callAddObstacles = [SKAction performSelector:@selector(addObstacles) onTarget:self];
+        SKAction *wait = [SKAction waitForDuration:1.5];
+        SKAction *waitThenAdd = [SKAction sequence:@[callAddObstacles,wait]];
+        self.obstacleLoop = [SKAction repeatActionForever:waitThenAdd];
+        
+        self.gameOverAlert = [[UIAlertView alloc] initWithTitle:@"Game Over"
+                                                   message:nil
+                                                  delegate:nil
+                                         cancelButtonTitle:@"OK"
+                                         otherButtonTitles:nil];
+        
+        self.startGameLabel = [SKLabelNode labelNodeWithFontNamed:@"Chalkduster"];
+        self.startGameLabel.text = @"Tap to play";
+        self.startGameLabel.fontSize = 15;
+        self.startGameLabel.position = CGPointMake(size.width/2, size.height/2);
+        
+        self.gameOver = NO;
+        
+        self.rectangle = [SKSpriteNode spriteNodeWithColor:[UIColor redColor] size:CGSizeMake(50, 50)];
+        [self setRectanglePositionAndAddToScene];
+        [self addChild:self.startGameLabel];
     }
     return self;
 }
 
 -(void)touchesBegan:(NSSet *)touches withEvent:(UIEvent *)event {
     /* Called when a touch begins */
-    //is rectangle different from nil
-    if (!self.rectangle) {
-        //get a single touch
-        UITouch *touch = [touches anyObject];
-        //get the location of the touch
-        CGPoint location = [touch locationInNode:self];
-        self.rectangle = [SKSpriteNode spriteNodeWithColor:[UIColor redColor] size:CGSizeMake(50, 50)];
-        //set the newly created node position to the position of the touch
-        self.rectangle.position = location;
-        //initialize the physicsBody with the size of the rectangle
-        self.rectangle.physicsBody = [SKPhysicsBody bodyWithRectangleOfSize:self.rectangle.size];
-        //gravity is going to be simulated on this node
-        self.rectangle.physicsBody.affectedByGravity = YES;
-        self.rectangle.physicsBody.mass = 0.5f;
-        //collision detection
-        self.rectangle.physicsBody.categoryBitMask = JCColliderTypeRectangle;
-        self.rectangle.physicsBody.collisionBitMask = JCColliderTypeObstacle | JCColliderTypeRectangle;
-        self.rectangle.physicsBody.contactTestBitMask = JCColliderTypeObstacle;
-        //adding the node to the scene
-        [self addChild:self.rectangle];
-        SKAction *callAddObstacles = [SKAction performSelector:@selector(addObstacles) onTarget:self];
-        SKAction *wait = [SKAction waitForDuration:1.5];
-        SKAction *waitThenAdd = [SKAction sequence:@[callAddObstacles,wait]];
-        SKAction *obstacleLoop = [SKAction repeatActionForever:waitThenAdd];
-        [self.rectangle runAction:obstacleLoop];
+    if (!self.rectangle.physicsBody||self.gameOver) {
+        if (!self.rectangle.physicsBody) {
+            [self.startGameLabel removeFromParent];
+            //initialize the physicsBody with the size of the rectangle
+            self.rectangle.physicsBody = [SKPhysicsBody bodyWithRectangleOfSize:self.rectangle.size];
+            //gravity is going to be simulated on this node
+            self.rectangle.physicsBody.affectedByGravity = YES;
+            self.rectangle.physicsBody.allowsRotation = NO;
+            self.rectangle.physicsBody.mass = 0.5f;
+            //collision detection
+            self.rectangle.physicsBody.categoryBitMask = JCColliderTypeRectangle;
+            self.rectangle.physicsBody.collisionBitMask = JCColliderTypeObstacle | JCColliderTypeRectangle;
+            self.rectangle.physicsBody.contactTestBitMask = JCColliderTypeObstacle;
+        }
+        if (self.gameOver) {
+            [self removeAllChildren];
+            self.paused = NO;
+            self.gameOver = NO;
+            [self setRectanglePositionAndAddToScene];
+        }
+        [self.rectangle runAction:self.obstacleLoop withKey:@"obstacles"];
     }
     else{
         [self.rectangle.physicsBody applyImpulse:CGVectorMake(0.0f, 250.0f)];
@@ -111,8 +133,17 @@ typedef enum : uint8_t {
     
     if ((contact.bodyA.categoryBitMask == JCColliderTypeRectangle && contact.bodyB.categoryBitMask == JCColliderTypeObstacle) || (contact.bodyB.categoryBitMask == JCColliderTypeRectangle && contact.bodyA.categoryBitMask == JCColliderTypeObstacle)) {
         self.paused = YES;
+        [self.rectangle removeActionForKey:@"obstacles"];
+        self.gameOver = YES;
+        [self.gameOverAlert show];
+        [self addChild:self.startGameLabel];
     }
 
+}
+
+-(void)setRectanglePositionAndAddToScene{
+    self.rectangle.position = CGPointMake(60, self.size.height/2);
+    [self addChild:self.rectangle];
 }
 
 @end
